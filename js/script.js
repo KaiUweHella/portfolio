2afb8ca587ff1bdb019bd67782997e4fcd95634b
KaiUweHella <kai.hella27@gmail.com>
2022-06-23 23:49:32 +0200

    added overlay and link to websites

---
+++ js/script.js
@@ -0,0 +1,43 @@
+function init() {
+  loadProjects();
+}
+
+function loadProjects() {
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].categorie == "ui-design") {
+      document.getElementById("ui-design").innerHTML += contentBoxHTML(i);
+      loadTools(i);
+    }
+    if (projects[i].categorie == "web-development") {
+      document.getElementById("web-development").innerHTML += contentBoxHTML(i);
+      loadTools(i);
+    }
+    if (projects[i].categorie == "creative") {
+      document.getElementById("creative").innerHTML += creativeImgHTML(i);
+    }
+  }
+}
+
+function loadTools(i) {
+  const tools = projects[i].tools;
+  for (let j = 0; j < tools.length; j++) {
+    const toolName = tools[j];
+    document.getElementById(`tools${i}`).innerHTML += toolsHTML(
+      `../img/tools/${toolName}.svg `
+    );
+  }
+}
+
+function showLinks(i) {
+  if (projects[i].categorie == "web-development") {
+    document.getElementById(`img${i}`).innerHTML += overlayLinksHTML(i);
+  }
+}
+
+function hideLinks(i) {
+  if (projects[i].categorie == "web-development") {
+    setTimeout(() => {
+      document.getElementById(`img${i}`).innerHTML = loadImgHTML(i);
+    }, 250);
+  }
+}
